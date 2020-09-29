b406750efd48a181767d816c05a3d4c221772a3a
Diego Doná <diedona@gmail.com>
2020-09-28 21:05:05 -0300

    Updates

---
+++ precache-manifest.161d4c83e9748b26fb00f77efbec85ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b971ee5197a79309d44b3ae9c2248304",
+    "url": "/react-apontamentos-app/index.html"
+  },
+  {
+    "revision": "d3f9b25b2a2ad0184b70",
+    "url": "/react-apontamentos-app/static/css/main.837266dd.chunk.css"
+  },
+  {
+    "revision": "d2ebd497d3fc09746f86",
+    "url": "/react-apontamentos-app/static/js/2.9061bbc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-apontamentos-app/static/js/2.9061bbc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3f9b25b2a2ad0184b70",
+    "url": "/react-apontamentos-app/static/js/main.623b2c58.chunk.js"
+  },
+  {
+    "revision": "06a3f0354b18109ed954",
+    "url": "/react-apontamentos-app/static/js/runtime-main.004bf660.js"
+  }
+]);
